9789f2069fd9859b8b45a51052be1d6f01be3226
Kauan Morinel Calheiro <kauan.calheiro@universo.univates.br>
2025-08-30 00:48:32 -0300

    style: adjust logo size

--- app/components/logo/kmc.vue
+++ app/components/logo/kmc.vue
@@ -1,6 +1,6 @@
 <template>
   <ULink class="flex justify-center items-center text-default" to="/">
-    <div class="relative inline-block font-oooh-baby text-4xl h-10 w-10">
+    <div class="relative inline-block font-oooh-baby text-3xl h-8 w-8">
       <span class="absolute inset-0">K</span>
       <span class="absolute inset-0">M</span>
       <span class="absolute inset-0">C</span>
