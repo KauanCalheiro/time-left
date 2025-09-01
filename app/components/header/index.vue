6ae554bb971f626db852cd908b974fd19cddca61
Kauan Morinel Calheiro <kauan.calheiro@universo.univates.br>
2025-09-01 11:42:35 -0300

    style: header fixed on top

--- app/components/header/index.vue
+++ app/components/header/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="w-full fixed md:sticky bottom-0 md:top-4 md:bottom-auto">
+    <header class="w-full fixed bottom-0 md:top-4 md:max-w-7xl z-100 md:px-2">
         <div class="flex justify-between items-center gap-4 py-2 my-4 mx-4 md:mx-0 px-6 bg-default rounded-2xl border-[0.5px] border-accented">
             <div>
                 <slot name="left">
