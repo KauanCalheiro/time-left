6424d4f78e31addf8acf00780b3e9879cf5457f9
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-09-08 14:51:59 -0300

    feat: add app components

---
+++ app/components/app/subtitle.vue
@@ -0,0 +1,5 @@
+<template>
+    <h2 class="text-2xl font-normal leading-tight tracking-tight text-text-secondary md:text-3xl">
+        <slot />
+    </h2>
+</template>
---
+++ app/components/app/title.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class="text-4xl font-medium leading-tight tracking-tighter text-text-primary md:text-6xl md:leading-[64px]">
+        <slot />
+    </h1>
+</template>
